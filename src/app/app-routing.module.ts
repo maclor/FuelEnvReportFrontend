b17da150557742cb6013f43ddb70bf505e92242a
Maciej Lorenc <maciej.lorenc@hicron.com>
2019-03-24 10:13:50 +0100

    Dashboard removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CompanyComponent} from "./company/company.component";
-import {VehicleComponent} from "./vehicle/vehicle.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
 import {CompanyDetailComponent} from "./company-detail/company-detail.component";
 import {InvoiceAddComponent} from "./invoice-add/invoice-add.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '', redirectTo: '/companies', pathMatch: 'full'},
   {path: 'companies', component: CompanyComponent},
   {path: 'company/:id', component: CompanyDetailComponent},
   {path: 'vehicle/:id', component: CompanyDetailComponent},
-  {path: 'dashboard', component: DashboardComponent},
   {path: 'invoice/add', component: InvoiceAddComponent},
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
-import {FormsModule} from "@angular/forms";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService} from './in-memory-data.service';
 
 import {CompanyComponent} from './company/company.component';
 import {CompanyDetailComponent} from './company-detail/company-detail.component';
 import {AppRoutingModule} from './app-routing.module';
-import {DashboardComponent} from './dashboard/dashboard.component';
 import {VehicleComponent} from './vehicle/vehicle.component';
 import {VehicleAddComponent} from './vehicle-add/vehicle-add.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {InvoiceAddComponent} from './invoice-add/invoice-add.component';
-import { InvoicesComponent } from './invoices/invoices.component';
-import { DateFormatterComponent } from './date-formatter/date-formatter.component';
+import {InvoicesComponent} from './invoices/invoices.component';
+import {DateFormatterComponent} from './date-formatter/date-formatter.component';
 
 @NgModule({
   imports: [
@@ -32,7 +30,6 @@ import { DateFormatterComponent } from './date-formatter/date-formatter.componen
     AppComponent,
     CompanyComponent,
     CompanyDetailComponent,
-    DashboardComponent,
     VehicleComponent,
     VehicleAddComponent,
     InvoiceAddComponent,
